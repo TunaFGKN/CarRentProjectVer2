12859742945b52d5e28f88f2d5a86b725a97c9d0
Tuna Figankaplan <swaxztn@gmail.com>
2023-09-22 03:19:36 +0300

    Brand and color controllers added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -18,6 +18,12 @@ namespace WebAPI
         {
             services.AddSingleton<ICarService,CarManager>();
             services.AddSingleton<ICarDal,EfCarDal>();
+
+            services.AddSingleton<IColorService,ColorManager>();
+            services.AddSingleton<IColorDal,EfColorDal>();
+
+            services.AddSingleton<IBrandService, BrandManager>();
+            services.AddSingleton<IBrandDal, EfBrandDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
