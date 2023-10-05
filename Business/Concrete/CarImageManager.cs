37b16258ef2188adbb70313453df525ed2dcf519
Tuna Figankaplan <swaxztn@gmail.com>
2023-10-06 00:47:30 +0300

    Car Images configuration half way done

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Core.Utilities.Helpers.FileHelper;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+        IFileHelper _fileHelper;
+        public CarImageManager(ICarImageDal carImageDal, IFileHelper fileHelper)
+        {
+            _carImageDal = carImageDal;
+            _fileHelper = fileHelper;
+        }
+        public IResult Add(IFormFile file, CarImage carImage)
+        {
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetByCarId(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetByImageId(int imageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
